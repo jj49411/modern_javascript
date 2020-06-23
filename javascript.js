594d8054ec943ad3f90fc82494fc01224f166d36
Julie <j49411@gmail.com>
2020-06-23 16:24:15 +0100

    feat: add scope examples

--- javascript.js
+++ javascript.js
@@ -10,3 +10,19 @@ login('Facebook', 1, 2, 3, 4)
 var arr1 = [4, 5, 6]
 var arr2 = [1, 2, 3, ...arr1]
 console.log(arr2) //  [1, 2, 3, 4, 5, 6]
+
+
+var a = 'hello'
+typeof(a) // string
+a = 1
+typeof(a) // number
+
+
+//block level
+{
+  let a = 'block'
+  {
+    console.log(a) // block
+  }
+}
+console.log(a) // a is not defined
